8c42286b95a14278cd692426d99730752fc8998e
zainsaleem367-netizen <zainsaleem367@gmail.com>
2026-07-12 22:22:26 +0400

    Add sw.js for Zain Finance mobile app

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'zain-finance-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (new URL(event.request.url).origin === location.origin) {
+      const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
